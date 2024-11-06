129451e95f71ac9387fa75144f814a139fbc0a7c
耗子 <i@haozi.net>
2024-11-07 00:09:37 +0800

    feat: add empty test

--- crypt/xchacha20poly1305_test.go
+++ crypt/xchacha20poly1305_test.go
@@ -22,6 +22,14 @@ func (s *XChacha20Poly1305TestSuite) TestEncrypt() {
 	s.NoError(err)
 }
 
+func (s *XChacha20Poly1305TestSuite) TestEncryptEmpty() {
+	encrypter, err := NewXChacha20Poly1305([]byte("12345678901234567890123456789012"))
+	s.NoError(err)
+	payload, err := encrypter.Encrypt([]byte(""))
+	s.NotEmpty(payload)
+	s.NoError(err)
+}
+
 func (s *XChacha20Poly1305TestSuite) TestDecrypt() {
 	encrypter, err := NewXChacha20Poly1305([]byte("12345678901234567890123456789012"))
 	s.NoError(err)
@@ -30,4 +38,14 @@ func (s *XChacha20Poly1305TestSuite) TestDecrypt() {
 	plaintext, err := encrypter.Decrypt(value)
 	s.NoError(err)
 	s.Equal("test", string(plaintext))
+}
+
+func (s *XChacha20Poly1305TestSuite) TestDecryptEmpty() {
+	encrypter, err := NewXChacha20Poly1305([]byte("12345678901234567890123456789012"))
+	s.NoError(err)
+	value, err := encrypter.Encrypt([]byte(""))
+	s.NoError(err)
+	plaintext, err := encrypter.Decrypt(value)
+	s.NoError(err)
+	s.Empty(plaintext)
 }
